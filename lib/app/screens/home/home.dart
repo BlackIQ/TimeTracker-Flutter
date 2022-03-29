d7d8e27385cff88c987fec17e54e7925dc217cf9
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-30 02:51:18 +0430

    Added home getch data | sample

--- lib/app/screens/home/home.dart
+++ lib/app/screens/home/home.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_time_tracker/app/services/flutterfire.dart';
 
@@ -11,6 +12,11 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final user = FirebaseAuth.instance.currentUser;
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  final Stream<QuerySnapshot> _usersStream = FirebaseFirestore.instance.collection('tasks').where('uid', isNotEqualTo: 'amir').snapshots();
+
+  final TextEditingController _name = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -33,8 +39,27 @@ class _HomeScreenState extends State<HomeScreen> {
         elevation: 0,
         title: const Text('Home'),
       ),
-      body: Center(
-        child: Text(user!.email.toString()),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: _usersStream,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return const Center(child: Text('Something went wrong'));
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          return snapshot.data!.docs.length > 0 ? ListView(
+            children: snapshot.data!.docs.map((DocumentSnapshot document) {
+              Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+              return ListTile(
+                title: Text(data['name']),
+                onTap: () {},
+              );
+            }).toList(),
+          ) : const Center(child: Text('No data'));
+        },
       ),
     );
   }
