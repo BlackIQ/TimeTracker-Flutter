02426b0c1f88c89bc72740ea6aa9f13dd826e1a2
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-29 23:30:54 +0430

    Add a landing page

--- lib/app/screens/landing.dart
+++ lib/app/screens/landing.dart
@@ -1,15 +1,37 @@
+import 'dart:async';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_time_tracker/app/screens/auth/auth.dart';
 import 'package:flutter_time_tracker/app/screens/home/home.dart';
 
-class Landing extends StatelessWidget {
-  final user = FirebaseAuth.instance.currentUser;
-
+class Landing extends StatefulWidget {
   Landing({Key? key}) : super(key: key);
 
   @override
+  State<Landing> createState() => _LandingState();
+}
+
+class _LandingState extends State<Landing> {
+  bool userState = false;
+
+  void checkUser() {
+    FirebaseAuth.instance
+        .authStateChanges()
+        .listen((User? user) {
+      if (user == null) {
+        setState(() {
+          userState = false;
+        });
+      } else {
+        userState = true;
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return user == null ? const AuthScreen() : const HomeScreen();
+    checkUser();
+    return userState ? const HomeScreen() : const AuthScreen();
   }
 }
