cd8da744fa6843abb20cff1932b0816c7357a554
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-29 14:50:45 +0430

    Move generated file in firebase dir and init app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_time_tracker/app/firebase/firebase_options.dart';
 import 'package:flutter_time_tracker/app/screens/landing.dart';
 
-void main() => runApp(const MyApp());
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
