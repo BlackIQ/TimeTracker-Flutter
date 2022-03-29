28c03dc1e69ca7220653313a46b5186d218fc004
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-29 19:27:13 +0430

    Added all new methods | Google

--- lib/app/services/flutterfire.dart
+++ lib/app/services/flutterfire.dart
@@ -1,1 +1,108 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+FirebaseAuth auth = FirebaseAuth.instance;
+
+Future<void> loginAuth(String email, String password, context) async {
+  try {
+    UserCredential userCredential = await auth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  } catch (error) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Something went wrong.'),
+          content: Text(error.toString()),
+          actions: <Widget>[
+            ElevatedButton(
+              child: const Text('Ok'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+Future<void> registerAuth(String email, String password, context) async {
+  try {
+    UserCredential userCredential = await auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  } catch (error) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Something went wrong.'),
+          content: Text(error.toString()),
+          actions: <Widget>[
+            ElevatedButton(
+              child: const Text('Ok'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+Future<void> googleAuth() async {}
+
+Future<void> logout(context) async {
+  try {
+    await auth.signOut();
+  } catch (error) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Something went wrong.'),
+          content: Text(error.toString()),
+          actions: <Widget>[
+            ElevatedButton(
+              child: const Text('Ok'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+Future<void> anonAuth(context) async {
+  try {
+    UserCredential userCredential = await auth.signInAnonymously();
+  } catch (error) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Something went wrong.'),
+          content: Text(error.toString()),
+          actions: <Widget>[
+            ElevatedButton(
+              child: const Text('Ok'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
