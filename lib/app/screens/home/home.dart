ade649d6914edc1dfe0470e1fc29dc8809dd3310
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-29 14:03:13 +0430

    Sample home

---
+++ lib/app/screens/home/home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 0,
+        title: const Text('Home'),
+      ),
+      body: const Center(
+        child: Text('Home'),
+      ),
+    );
+  }
+}
