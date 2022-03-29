20a01bd5642b01f256ad1ea01ec2c2eefbb0c93c
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-29 19:26:47 +0430

    Add logout

--- lib/app/screens/home/home.dart
+++ lib/app/screens/home/home.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_time_tracker/app/services/flutterfire.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,16 +10,31 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final user = FirebaseAuth.instance.currentUser;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          TextButton(
+            child: const Text(
+              'Logout',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            onPressed: () async {
+              await logout(context);
+            },
+          ),
+        ],
         centerTitle: true,
         elevation: 0,
         title: const Text('Home'),
       ),
-      body: const Center(
-        child: Text('Home'),
+      body: Center(
+        child: Text(user!.email.toString()),
       ),
     );
   }
