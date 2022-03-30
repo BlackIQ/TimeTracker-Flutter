cf3f08b6f9b42d0bce2bdb1deb14f586c065c48a
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-30 11:13:51 +0430

    Remove an unused import

--- lib/app/screens/landing.dart
+++ lib/app/screens/landing.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_time_tracker/app/screens/auth/auth.dart';
